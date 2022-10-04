7e9fa3d7aaaeed47505f8fad413b376e7dc93aad
Frisle <47441164+Frisle@users.noreply.github.com>
2022-10-04 14:41:08 +0600

    telegramm-bot project update

--- dodo_parse.py
+++ dodo_parse.py
@@ -4,8 +4,6 @@ import time
 import os
 
 
-
-
 url = "https://dodopizza.kz/petropavlovsk"
 html_file = "saved_page.html"
 
@@ -31,150 +29,126 @@ def try_get_page():
         print("File does`nt exist")
 
 
+def main(product_type, ingredience="С мясом"):
+    def read_menu():
+        with open(html_file, "r", encoding="utf-8") as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, "lxml")
+
+        drinks = "drinks"
+
+        title = []
+        description = []
+        drinks_title = []
+        drinks_description = []
+        avilability_list_food = []
+        avilability_list_drinks = []
+
+        for titles in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh",
+                                attrs={"id": product_type}).find_all("div", class_="sc-1tpn8pe-1 jbQjVh"):
+            title.append(titles.text)
+        for desk in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh",
+                              attrs={"id": product_type}).find_all("main", class_="sc-1tpn8pe-0 jDJosZ"):
+            description.append(desk.text.strip("\n"))
+
+        for drinks_titles in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh",
+                                       attrs={"id": drinks}).find_all("div", class_="sc-1tpn8pe-1 jbQjVh"):
+            drinks_title.append(drinks_titles.text)
+        for drinks_desc in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh",
+                                            attrs={"id": drinks}).find_all("main", class_="sc-1tpn8pe-0 jDJosZ"):
+            drinks_description.append(drinks_desc.text)
+
+        for avilability_drinks in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh", attrs={"id": drinks}):
+            for button in avilability_drinks.find_all("button", class_="sc-1rmt3mq-0 jFZvXc product-control"):
+                if button.text == "В корзину":
+                    avilability_list_drinks.append("В наличии")
+                elif button.text == "Будет позже":
+                    avilability_list_drinks.append("Временно отсутствует")
+
+        for avilability in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh", attrs={"id": product_type}):
+            for button in avilability.find_all("button", class_="sc-1rmt3mq-0 jFZvXc product-control"):
+                if button.text == "Собрать" or button.text == "Выбрать" or button.text == "В корзину":
+                    avilability_list_food.append("В наличии")
+                elif button.text == "Будет позже":
+                    avilability_list_food.append("Временно отсутствует")
+
+        new_list = []
+        for desk, avail in zip(description, avilability_list_food):
+            new_list.append(f"{desk} @{avail}@\n")
+
+        complite_drinks_list = []
+        for titles, avail in zip(drinks_title, avilability_list_drinks):
+            complite_drinks_list.append(f"{titles} @{avail}@\n")
+
+        cut_description = []
+
+        n = 0
+        while n < len(new_list):
+            for item in title:
+                cut_description.append(new_list[n][len(item):])
+                n += 1
+
+        full_menu = dict(zip(title, cut_description))
+
+        return cut_description, full_menu, complite_drinks_list, avilability_list_food
+
+    def sort_menu():
+        full_menu = read_menu()
+        meat_menu = []
+        vegan_menu = full_menu[0].copy()
+
+        forbiden_words = ["вет", "цып", "инд", "индейки", "чор", "гов", "говядина", "пастр", "кур", "курица", "крыл"]
+
+        for elem in full_menu[0]:
+            for n in forbiden_words:
+                if n in elem.lower().replace(",", ""):
+                    meat_menu.append(elem)
+                    break
+
+        for elem in meat_menu:
+            if elem in vegan_menu:
+
+                vegan_menu.remove(elem)
+        print(meat_menu)
+
+        return vegan_menu, meat_menu
+    sort_menu()
 
 
+    def get_key(val):
+        full_menu = read_menu()
 
-def read_menu():
-    with open(html_file, "r", encoding="utf-8") as file:
-        src = file.read()
+        for key, value in full_menu[1].items():
+            if val == value:
+                return key
 
-    soup = BeautifulSoup(src, "lxml")
-
-
-    snacks = "snacks"
-    pizzas = "pizzas"
-    drinks = "drinks"
-
-    title = []
-    description = []
-    drinks_title = []
-    drinks_description = []
-    avilability_list_food = []
-    avilability_list_drinks = []
-
-    file = open("product.txt", "r")
-    product_type = file.read()
-    file.close()
-
-    for titles in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh",
-                            attrs={"id": product_type}).find_all("div", class_="sc-1tpn8pe-1 jbQjVh"):
-        title.append(titles.text)
-    for desk in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh",
-                          attrs={"id": product_type}).find_all("main", class_="sc-1tpn8pe-0 jDJosZ"):
-        description.append(desk.text.strip("\n"))
-
-    for drinks_titles in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh",
-                                   attrs={"id": drinks}).find_all("div", class_="sc-1tpn8pe-1 jbQjVh"):
-        drinks_title.append(drinks_titles.text)
-    for drinks_desc in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh",
-                                        attrs={"id": drinks}).find_all("main", class_="sc-1tpn8pe-0 jDJosZ"):
-        drinks_description.append(drinks_desc.text)
-
-    for avilability_drinks in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh", attrs={"id": drinks}):
-        for button in avilability_drinks.find_all("button", class_="sc-1rmt3mq-0 jFZvXc product-control"):
-            if button.text == "В корзину":
-                avilability_list_drinks.append("В наличии")
-            elif button.text == "Будет позже":
-                avilability_list_drinks.append("Временно отсутствует")
-
-    for avilability in soup.find("section", class_="sc-1n2d0ov-2 bxiXBh", attrs={"id": product_type}):
-        for button in avilability.find_all("button", class_="sc-1rmt3mq-0 jFZvXc product-control"):
-            if button.text == "Собрать" or button.text == "Выбрать" or button.text == "В корзину":
-                avilability_list_food.append("В наличии")
-            elif button.text == "Будет позже":
-                avilability_list_food.append("Временно отсутствует")
-
-    new_list = []
-    for desk, avail in zip(description, avilability_list_food):
-        new_list.append(f"{desk} @{avail}@\n")
-
-    complite_drinks_list = []
-    for titles, avail in zip(drinks_title, avilability_list_drinks):
-        complite_drinks_list.append(f"{titles} @{avail}@\n")
+        return "key doesn't exist"
 
 
-    # drinks_cut_description_unfiltered = []
-    cut_description = []
+    def print_request_menu():
 
+        request_menu = sort_menu()
+        drinks_title = read_menu()
 
-    n = 0
-    while n < len(new_list):
-        for item in title:
-            cut_description.append(new_list[n][len(item):])
-            n += 1
-
-
-    # p = 0
-    # while p < len(drinks_description):
-    #     for item in drinks_title:
-    #         if len(drinks_description) != 0:
-    #             drinks_cut_description_unfiltered.append(drinks_description[p][len(item):])
-    #         p += 1
-    # drinks_cut_description = list(filter(None, drinks_cut_description_unfiltered))
-
-
-
-    full_menu = dict(zip(title, cut_description))
-
-    return cut_description, full_menu, complite_drinks_list, avilability_list_food
-
-
-
-
-
-def sort_menu():
-    full_menu = read_menu()
-    meat_menu = []
-    vegan_menu = full_menu[0].copy()
-
-    forbiden_words = ["вет", "цып", "инд", "индейки", "чор", "гов", "говядина", "пастр", "кур", "курица", "крыл"]
-
-    for elem in full_menu[0]:
-        for n in forbiden_words:
-            if n in elem.lower().replace(",", ""):
-                meat_menu.append(elem)
-                break
-
-    for elem in meat_menu:
-        if elem in vegan_menu:
-
-            vegan_menu.remove(elem)
-
-
-    return vegan_menu, meat_menu
-sort_menu()
-
-
-def get_key(val):
-    full_menu = read_menu()
-
-    for key, value in full_menu[1].items():
-        if val == value:
-            return key
-
-    return "key doesn't exist"
-
-
-def print_request_menu(ingredience):
-
-    request_menu = sort_menu()
-    drinks_title = read_menu()
-
-    result = ""
-    if ingredience == "Без мяса":
-        for item in request_menu[0]:
-            result += f"\n {get_key(item)}-{item}"
-        return result
-    elif ingredience == "С мясом":
-        for item in request_menu[1]:
-            result += f"\n {get_key(item)}-{item}"
-        return result
-    elif ingredience == "Напитки":
-        for drinks in drinks_title[2]:
-            result += f"{drinks}"
-        return result
-# print_request_menu()
-
+        result = ""
+        if ingredience == "Без мяса":
+            for item in request_menu[0]:
+                result += f"\n {get_key(item)}-{item}"
+            return result
+        elif ingredience == "С мясом":
+            for item in request_menu[1]:
+                result += f"\n {get_key(item)}-{item}"
+            return result
+        elif ingredience == "Напитки":
+            for drinks in drinks_title[2]:
+                result += f"{drinks}"
+            return result
+        elif ingredience == "Десерты":
+            for item in request_menu[0]:
+                result += f"\n {get_key(item)}-{item}"
+            return result
+    return print_request_menu()
 
 
 
--- main.py
+++ main.py
@@ -1,72 +1,58 @@
+from telebot import types
 import telebot
 from auth_data import token
-from link_analizator import ticker
-from dodo_parse import print_request_menu
+from dodo_parse import main
 from dodo_parse import try_get_page
 
-about = """
-Прямо сейчас не много вещей
-одна из основных задач тестировать код
-для построения телеграмм-бота
-"""
 
-
-
-
-ticker = ticker()
-emoji_money = "\U0001F911"
 info = try_get_page()
 
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
 
+
     @bot.message_handler(commands=["start"])
     def start_message(message):
-        bot.send_message(message.chat.id, "Тест-бот готов работать для пирожка! Напишите \"Без мяса\" чтобы получить"
-                                          " веганские закуски. Напишите \" С мясом\" чтобы получить закуски с мясом из До-До."
-                                          " Напишите \" Напитки \" чтобы получить меню напитков")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Пицца:Без мяса:pizzas')
+        btn2 = types.KeyboardButton("Пицца:С мясом:pizzas")
+        btn3 = types.KeyboardButton("Закуска:Без мяса:snacks")
+        btn4 = types.KeyboardButton("Закуска:С мясом:snacks")
+        btn5 = types.KeyboardButton("Напитки:Напитки:drinks")
+        btn6 = types.KeyboardButton("Десерты:Десерты:desserts")
 
-    @bot.message_handler(commands=["snacks"])
-    def snacks_switch(message):
-        bot.send_message(message.chat.id, "Выбраны закуски")
-        file = open("product.txt", "w")
-        file.write("snacks")
-        file.close()
 
-    @bot.message_handler(commands=["pizza"])
-    def snacks_switch(message):
-        bot.send_message(message.chat.id, "Выбраны пиццы")
-        file = open("product.txt", "w")
-        file.write("pizzas")
-        file.close()
+        markup.add(btn1)
+        markup.add(btn2)
+        markup.add(btn3)
+        markup.add(btn4)
+        markup.add(btn5)
+        markup.add(btn6)
+
+        bot.send_message(message.chat.id, "Тест-бот готов работать для пирожка! "
+                                          "В меню бота можно выбрать весь ассортимент "
+                                          "До-До пиццерии", reply_markup=markup)
+
 
     @bot.message_handler(content_types=["text"])
     def message_handler(message):
-        if message.text == "Без мяса":
+        print(message.text.split(":"))
+        print(message.text.split(":"))
+        if message.text:
             bot.send_message(message.chat.id, info)
-            menu = print_request_menu("Без мяса")
-            bot.send_message(message.chat.id, "Меню без мяса на сегодня\n")
+            menu = main(message.text.split(":")[2], message.text.split(":")[1])
+            bot.send_message(message.chat.id, "Меню на текущий час\n")
             bot.send_message(message.chat.id, menu)
-        elif message.text == "С мясом":
-            bot.send_message(message.chat.id, info)
-            menu = print_request_menu("С мясом")
-            bot.send_message(message.chat.id, "Меню с мясом на сегодня\n")
-            bot.send_message(message.chat.id, menu)
-        elif message.text == "Напитки":
-            bot.send_message(message.chat.id, info)
-            menu = print_request_menu("Напитки")
-            bot.send_message(message.chat.id, "Меню напитков на сегодня\n")
-            bot.send_message(message.chat.id, menu)
+            bot.send_message(message.chat.id, "https://dodopizza.kz/")
+        # elif message.text == "Напитки:drinks":
+        #      bot.send_message(message.chat.id, info)
+        #      menu = main(message.text.split(":")[1])
+        #      bot.send_message(message.chat.id, "Меню напитков на сегодня\n")
+        #      bot.send_message(message.chat.id, menu)
 
         else:
-            bot.send_message(message.chat.id, "Упс! Неверная комманда, попробуй еще раз")
-
-
-        # elif message.text == "кот":
-        #     bot.send_message(message.chat.id, "https://klike.net/uploads/posts/2021-05/1621412147_3.jpg")
-
-
+            bot.send_message(message.chat.id, "Упс! Неверная команда, попробуй еще раз")
 
 
 
